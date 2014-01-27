a648849bbd33fbf4d870f0f9857e9989eb01eb93
slh012 <seanhardaker@gmail.com>
2014-01-27 17:43:03 +0000

    Removed unnecessary commented out code

--- public/header.inc.php
+++ public/header.inc.php
@@ -6,7 +6,7 @@ function getTime() {
 }
 
      $start = getTime(); 
-//Read more: http://www.webdesign.org/web-programming/php/benchmark-and-optimize-php-script-speed.14875.html#ixzz2bGazWeOl
+
 
 	set_include_path('../classes/examplesite'.PATH_SEPARATOR.'../classes/examplesite/datadictionary'.PATH_SEPARATOR.'../classes/datadictionary'.PATH_SEPARATOR.'../classes/downloading'.PATH_SEPARATOR.'../classes/forms'.PATH_SEPARATOR.'../classes/db'.PATH_SEPARATOR.'../classes/exceptions'.PATH_SEPARATOR.'../classes');
 	include_once('init.inc.php');
