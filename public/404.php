bc4c10b97a57182791016ad810513f8a0410bd68
slh012 <seanhardaker@gmail.com>
2014-01-27 17:43:33 +0000

    Update 404.php

--- public/404.php
+++ public/404.php
@@ -3,8 +3,8 @@ try{
  
 	include_once('header.inc.php');
 
-	page::assign('header1','Link Liability');
-	page::assign('title','Link Liability');
+
+	page::assign('title','PAGE TITLE UNSET');
         page::assignLoadFile('content', 'htmlblocks/404.html');
 	
         include_once('footer.inc.php');
